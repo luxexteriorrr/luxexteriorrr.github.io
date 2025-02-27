6995a72a0d6a61df8b5f242aae48182480ae1814
Kirill Konstantinov <kirkon3@gmail.com>
2025-02-27 15:20:11 +0000

    fixed the grid

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    //smothing the flexboxes for webkit compat
     document.querySelectorAll('.grid-element').forEach(el => {
         el.addEventListener('mouseenter', () => {
             el.style.flexGrow = 3;
@@ -7,4 +8,14 @@ document.addEventListener('DOMContentLoaded', () => {
             el.style.flexGrow = 1;
         });
     });    
+
+    //effects of the text
+    document.querySelectorAll('.grid-element').forEach(el => {
+        el.addEventListener('mouseenter', () => {
+            el.querySelector('.project').classList.add('active')
+        })
+        el.addEventListener('mouseleave', () => {
+            el.querySelector('.project').classList.remove('active')
+        })
+    })
 })
